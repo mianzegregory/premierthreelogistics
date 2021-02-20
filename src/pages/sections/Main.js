e748b982afd0c9c8832d9e4367283ca0763e796c
Greg <mianzegregory@outlook.com>
2021-02-20 14:53:31 +0300

    styled ain section

--- src/pages/sections/Main.js
+++ src/pages/sections/Main.js
@@ -6,11 +6,19 @@ const MainSection = () => {
   return (
     <SectionWrapper>
       <Col>
-        <h1>PREMIER THREE LOGISTICS</h1>
-        <p>One stop solution for all your logistics And shipping needs.</p>
+        <h1>
+          PREMIER THREE <br />
+          LOGISTICS
+        </h1>
+        <br />
+        <p>
+          One stop solution for all your logistics <br /> And shipping needs.
+        </p>
+        <br />
+        <br />
         <div className="btn-row">
-          <BTN>CONTACT US</BTN>
-          <BTN>LEARN MORE</BTN>
+          <ContactBTN>CONTACT US</ContactBTN>
+          <LearnMoreBTN>LEARN MORE</LearnMoreBTN>
         </div>
       </Col>
       <Col>
@@ -24,13 +32,12 @@ export default MainSection;
 
 //styles for the section
 const SectionWrapper = styled.section`
-  padding: 0 5%;
+  padding: 0 10%;
   width: 100vw;
   height: 100vh;
   display: flex;
   flex-direction: row;
   justify-content: center;
-  align-items: center;
 `;
 
 //Column
@@ -39,21 +46,38 @@ const Col = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+ 
+  h1,
+  p {
+    text-align: left;
+    line-height: 1.6;
+  }
+  h1 {
+    font-size: 3rem;
+    font-family: "Ubuntu";
+    font-weight: 600;
+  }
+  p {
+    font-size: 1.5rem;
+    color: #7d7d7d;
+    font-family: open-sans;
+  }
   .btn-row {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    margin-right: 45%;
   }
 `;
 
-//Contact button
-const BTN = styled.button`
-  border-radius: 2rem;
-  padding: 0.5rem;
-  border: none;
-  font-size: 0.9rem;
+//contact button
+const ContactBTN = styled.button`
+  border-radius: 0.2rem;
+  padding: 0.7rem 1rem;
+  border: solid 2px #31b0c9;
+  font-size: 1rem;
+  font-weight: 600;
   text-decoration: none;
   display: inline-block;
   cursor: pointer;
@@ -62,6 +86,25 @@ const BTN = styled.button`
   transition: 0.2s;
   font-weight: 600;
   :hover {
-    background-color: #227a8b;
+    font-size: 1.2rem;
+  }
+`;
+
+//learn more button
+const LearnMoreBTN = styled.button`
+  border-radius: 0.2rem;
+  padding: 0.7rem 1rem;
+  border: solid 2px #31b0c9;
+  font-size: 1rem;
+  font-weight: 600;
+  text-decoration: none;
+  display: inline-block;
+  cursor: pointer;
+  color: #31b0c9;
+  background-color: #fff;
+  transition: 0.2s;
+  font-weight: 600;
+  :hover {
+    font-size: 1.2rem;
   }
 `;
