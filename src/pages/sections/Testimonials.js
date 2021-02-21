39c766e9ff824d291368c5f1deda3e9a9df66d35
Greg <mianzegregory@outlook.com>
2021-02-21 13:49:09 +0300

    commennt skeleton ... style and replace with actual info

--- src/pages/sections/Testimonials.js
+++ src/pages/sections/Testimonials.js
@@ -5,6 +5,26 @@ const TestimonialSection = () => {
   return (
     <SectionWrapper>
       <h2>CLIENT TESTIMONIALS</h2>
+      <TestimonialCard>
+        <div className="col1" id="person-details">
+          <div className="row">
+            <div className="col">
+              <img
+                className="avatar"
+                src="https://ik.imagekit.io/sgmianze96/PremierThreeLogistics/team/greg_6GeRaeR1Qv-b.png"
+                alt="Steeve Gregory Mianze"
+              />
+            </div>
+            <div className="col">
+              <h4>Person's name</h4>
+              <p>Person's title</p>
+            </div>
+          </div>
+        </div>
+        <div className="col1" id="person-comment">
+          <p>Lorem ipsum lol... person's comment about the company</p>
+        </div>
+      </TestimonialCard>
     </SectionWrapper>
   );
 };
@@ -29,5 +49,38 @@ const SectionWrapper = styled.section`
   }
   * {
     background-color: transparent;
+  }
+`;
+
+//person card
+const TestimonialCard = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  .avatar {
+    vertical-align: middle;
+    width: 3em;
+    height: 3rem;
+    border-radius: 50%;
+    border: solid 2px #000;
+    bacground-color: blue;
+  }
+  .col1 {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+  }
+  .row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .col {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 0.5rem;
   }
 `;
